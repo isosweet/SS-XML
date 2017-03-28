c56665252d5fd1258eb8d6bea7b58f68d82fcb0a
isosweet <405438526@qq.com>
2017-03-28 18:06:41 +0800

    @ModelAttribute

--- src/main/java/com/wishfulcloud/ss/web/UserController.java
+++ src/main/java/com/wishfulcloud/ss/web/UserController.java
@@ -1,9 +1,13 @@
 package com.wishfulcloud.ss.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.wishfulcloud.ss.entity.User;
 
 /**   
@@ -15,7 +19,16 @@ import com.wishfulcloud.ss.entity.User;
  *
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
+	
+	@RequestMapping(value = "/saveUser", method = RequestMethod.POST)
+	public @ResponseBody String saveUser(@ModelAttribute("user") User user){
+		
+		System.out.println(JSON.toJSONString(user));
+		
+		return "success";
+	}
 	
 	
 	@RequestMapping(value = "/getUser")
@@ -27,4 +40,8 @@ public class UserController {
 		
 		return user;
 	}
+	
+	
+	
+	
 }
